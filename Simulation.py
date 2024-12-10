81d4bad3705cec7f7316354486460f4206aa915c
BeekerTheCat <bret.harder+beeker@harder.consulting>
2024-12-10 01:06:27 -0600

    Core template created

---
+++ Simulation.py
@@ -0,0 +1,18 @@
+# STANDARD IMPORTS
+from __future__ import annotations
+# CUSTOM IMPORTS
+
+
+# PARAMETERS
+# FUNCTIONS
+# CLASSES
+class BellSim:
+    vv: bool
+    pp: bool
+
+    def __init__(self, vv: bool, pp: bool):
+        self.vv = vv
+        self.pp = pp
+
+
+# EOF
--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 # STANDARD IMPORTS
+from __future__ import annotations
 # CUSTOM IMPORTS
+from Simulation import BellSim
+
+
 # PARAMETERS
+STATE_LOOKUP_DATABASE = "states.db"
+TRIAL_LOOKUP_DATABASE = "trials.db"
+VERBOSE = True
+PAUSE_PROMPT = True
+
+
 # FUNCTIONS
 # CLASSES
+
+
 # MAIN
-def main():
-    pass
+def main(state_db: str, trial_db: str, verbose: bool, pause_prompt: bool):
+    sim = BellSim(vv=verbose, pp=pause_prompt)
 
 
 # RUN
 if __name__ == "__main__":
-    main()
+    parameters = [
+        STATE_LOOKUP_DATABASE,
+        TRIAL_LOOKUP_DATABASE,
+        VERBOSE,
+        PAUSE_PROMPT
+    ]
+    main(*parameters)
 
 
 # EOF
